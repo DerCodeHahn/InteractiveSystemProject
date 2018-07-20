8a4c4c3a1ab0b7a504fac407c36340abcf9d44c4
DerCodeHahn <derhahn91@web.de>
2018-07-20 21:32:39 +0200

    ProjektStrucktur geändert 😇

--- unity-environment/Assets/BoatAgent.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using MLAgents;
-using UnityEngine;
-
-public class BoatAgent : Agent {
-
-}
---
+++ unity-environment/Assets/SeaBattle/Scripts/BoatAgent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MLAgents;
+using UnityEngine;
+
+public class BoatAgent : Agent 
+{
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        
+    }
+}
