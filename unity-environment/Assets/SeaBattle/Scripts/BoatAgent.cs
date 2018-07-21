6772132f61ab1afe6d618b365e184435311eace0
Khaled <khaled.reguieg@artcom.de>
2018-07-21 11:45:22 +0200

    :bomb: :bomb: yeah made shooting Work :bomb: :bomb:

--- unity-environment/Assets/SeaBattle/Scripts/BoatAgent.cs
+++ unity-environment/Assets/SeaBattle/Scripts/BoatAgent.cs
@@ -7,40 +7,60 @@ public class BoatAgent : Agent
 {
     private SeaManager seaManager;
 
-    private float speed = 0.25f;
+    private float speed = 10;
 
     [SerializeField]
     private GameObject canonBall;
-    private float canonBallSpeed = 2.0f;
+    private float canonBallSpeed = 1000.0f;
 
+    private float ShootingSpeed = 4f;
+    private float shootTimer = 0;
     private void Start()
     {
         seaManager = GetComponentInParent<SeaManager>();
     }
 
+
+
     public override void AgentAction(float[] vectorAction, string textAction)
     {
         Vector3 direction = new Vector3(vectorAction[1], 0, vectorAction[0]);
         direction.Normalize();
-        GetComponent<CharacterController>().Move(direction * speed);
-        transform.forward = direction;
-
-        Vector3 shotDirection = new Vector3(vectorAction[2], 0, vectorAction[2]);
-        GameObject canonball = Instantiate(canonBall, gameObject.transform.position, canonBall.transform.rotation);
-        canonBall.GetComponent<Rigidbody>().AddForce(shotDirection * canonBallSpeed);
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        GetComponent<Rigidbody>().AddForce(direction * speed, ForceMode.VelocityChange);
+        //GetComponent<CharacterController>().Move(direction* speed);
+        if(direction != Vector3.zero)
+            transform.forward = direction;
+        float shootDirection = vectorAction[2];
+        if(shootDirection != 0 && shootTimer >= 1/ShootingSpeed)
+        {
+            GameObject ccc = Instantiate(canonBall, transform.position, Quaternion.identity);
+			ccc.GetComponent<Rigidbody>().AddForce(transform.right * shootDirection * canonBallSpeed);
+            Physics.IgnoreCollision(GetComponent<Collider>(), ccc.GetComponent<Collider>());
+            Destroy(ccc,0.5f);
+            shootTimer = 0;
+        }
+    }
+    
+    void FixedUpdate(){
+        shootTimer += Time.deltaTime;
     }
 
     public override void CollectObservations()
     {
-        BoatAgent opponent = (this == seaManager.BoatAgents[0]) ? seaManager.BoatAgents[1] : seaManager.BoatAgents[0];
-        Vector3 enemyDirection = opponent.transform.position - transform.position;
-        AddVectorObs(enemyDirection);
+        Vector2 direction = seaManager.GetOther(this).transform.position - transform.position;
+        AddVectorObs(direction);
     }
 
-    void OnCollsionEnter(Collision col)
+    void OnTriggerEnter(Collider col)
     {
-        if(col.gameObject.CompareTag("ball"))
-            Debug.Log("Hit by canonball! gulp gulp gulp!");
+        
+        if(col.gameObject.tag == "ball"){
+            Destroy(col.gameObject);
+            ResetReward();
+            seaManager.GetOther(this).AddReward(1);
+            seaManager.Reset();
+        }
     }
 
 
--- unity-environment/Assets/SeaBattle/Scripts/SeaManager.cs
+++ unity-environment/Assets/SeaBattle/Scripts/SeaManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SeaManager : MonoBehaviour {
 
 	private BoatAgent[] boatAgents = new BoatAgent[2];
+    public float spawnRadius = 0.5f;
 
     public BoatAgent[] BoatAgents
     {
@@ -24,4 +25,20 @@ public class SeaManager : MonoBehaviour {
     void Start () {
 		boatAgents = GetComponentsInChildren<BoatAgent>();
 	}
+
+    public void Reset(){
+        foreach (BoatAgent boatAgent in boatAgents)
+        {
+            boatAgent.transform.position = Vector3.zero;
+            Vector2 rnd = Random.insideUnitCircle * spawnRadius;
+            boatAgent.transform.position += new Vector3(rnd.x,0,rnd.y);
+        }
+    }
+
+    public BoatAgent GetOther(BoatAgent agent){
+        if(boatAgents[0] == agent)
+            return boatAgents[1];
+        else
+            return boatAgents[0];
+    }
 }
